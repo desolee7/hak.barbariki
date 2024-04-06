e5f7086191ffb28d9d899f3358e96fc6aef53538
szznv <120127927+szznv@users.noreply.github.com>
2024-04-06 16:41:21 +0300

    button download

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 // тут была Аня
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,7 +10,7 @@ void main() {
 
 // ignore: use_key_in_widget_constructors, must_be_immutable
 class MyApp extends StatelessWidget {
-  List<String> items = ['One', 'Two', 'Three', 'Four'];
+  List<String> items = ['One', 'Two'];
   String? selectedItem = 'One';
 
   @override
@@ -23,7 +25,6 @@ class MyApp extends StatelessWidget {
             width: 100.0,
             height: 100.0,
             child: Container(
-              // color: Colors.red,
               width: 200.0,
               height: 50.0,
               decoration: BoxDecoration(
@@ -41,7 +42,8 @@ class MyApp extends StatelessWidget {
           margin: const EdgeInsets.symmetric(
               // horizontal: 20.0,
               ),
-          child: Row(
+          child:
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Column(
@@ -52,9 +54,8 @@ class MyApp extends StatelessWidget {
                     style: TextStyle(
                         fontSize: 68.0,
                         fontWeight: FontWeight.bold,
-                        color: Color.fromRGBO(0, 0, 0, 0.75),
-                        fontFamily: "Montserrat",
-                        fontStyle: FontStyle.italic),
+                        color: Color.fromRGBO(0, 0, 0, 0.50),
+                        fontFamily: "Montserrat"),
                   ),
                   Row(
                     children: [
@@ -63,25 +64,26 @@ class MyApp extends StatelessWidget {
                         style: TextStyle(
                           fontSize: 40.0,
                           fontWeight: FontWeight.bold,
-                          color: Color(0xff000000),
+                          color: Color.fromRGBO(0, 0, 0, 0.80),
                           fontFamily: "Montserrat",
                         ),
                       ),
-                      SizedBox(width: 5),
-                      Image.asset(
-                        "lib/img/download_icon.png",
-                        width: 50,
-                        height: 50,
-                      ),
+                      SizedBox(width: 15),
+                      IconButton(onPressed: () {}, icon: const Icon(Icons.download, color: Colors.white70,),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromRGBO(143, 124, 112, 1),
+                          shadowColor: Color(0xff000000)
+                        )),
                     ],
                   ),
-                  ElevatedButton(
+                  ElevatedButton.icon(
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color.fromRGBO(143, 124, 112, 1),
                       shadowColor: Color(0xff000000),
                     ),
-                    child: Text(
+                    icon: const Icon(Icons.search_sharp, color: Colors.white70,),
+                    label: Text(
                       "Поиск предметов",
                       style: TextStyle(
                         fontSize: 32.0,
@@ -91,6 +93,7 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                   // Spacer(flex: 1),
+
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
@@ -124,45 +127,33 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
-              Column(
-                children: [
+          Column(
+              children: [
                   Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(143, 124, 112, 1),
-                      border: Border.all(color: Color.fromRGBO(0, 0, 0, 0.2)),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: DropdownButton(
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        color: Color(0xffffffff),
-                        fontFamily: "Montserrat",
-                        fontStyle: FontStyle.italic,
-                        // fontWeight: FontWeight.w600,
-                        letterSpacing: 1.0,
-                      ),
-                      dropdownColor: Color.fromRGBO(143, 124, 112, 1),
-                      elevation: 2,
-                      underline: SizedBox(),
-                      items: items
-                          .map((item) =>
-                              DropdownMenuItem(value: item, child: Text(item)))
-                          .toList(),
-                      value: selectedItem,
-                      onChanged: (item) => setState(() => selectedItem = item),
-                    ),
+                  decoration: BoxDecoration(
+                  color: Color.fromRGBO(143, 124, 112, 1),
+                  border: Border.all(color: Color.fromRGBO(0, 0, 0, 0.2)),
+                  borderRadius: BorderRadius.circular(8.0),
                   ),
-                  // SizedBox( height: 10.0),
-                  // TextField(
-                  //   obscureText: true,
-                  //   decoration: InputDecoration(
-                  //     border: OutlineInputBorder(),
-                  //     labelText: 'Password',
-                  //   ),
-                  // )
+                  child: DropdownButton(
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Color(0xffffffff),
+                    fontFamily: "Montserrat",
+                    // fontWeight: FontWeight.w600,
+                    letterSpacing: 1.0,
+                  ),
+                  icon: const Icon(Icons.arrow_drop_down, color: Colors.white70,),
+                  dropdownColor: Color.fromRGBO(143, 124, 112, 1),
+                  elevation: 2,
+                  underline: SizedBox(),
+                  items: items.map((item) => DropdownMenuItem(value: item, child: Text(item))).toList(),
+                  value: selectedItem,
+                  onChanged: (item) => setState(() => selectedItem = item),
+                ),),
                 ],
               ),
-            ],
+            ]
           ),
         ),
       ),
