f5a01497ae5431a6572f0ffc25b59fb1c4587b2e
szznv <120127927+szznv@users.noreply.github.com>
2024-04-13 20:18:01 +0300

    last

--- lib/main.dart
+++ lib/main.dart
@@ -34,12 +34,14 @@ class MyApp extends State<Widget1> {
   List<String> items = [' Скульптура', ' Живопись'];
   String selectedItem = ' Скульптура';
   String textFieldValue = '';
+  String resp = "";
   File? _pickedImage;
   Uint8List webImage = Uint8List(8);
 
   final Logger logger = Logger(); //логи не работают яхз пофиг)))
 
-  String serverText = ''; // текст, полученный от сервера
+  String serverText = "Категория"; // текст, полученный от сервера
+  String serverDesc = "Здесь будет описание предмета...";
   List<String> serverPhotos = []; // список фотографий, полученных от сервера
 
   Future<void> _pickImage() async {
@@ -60,7 +62,7 @@ class MyApp extends State<Widget1> {
       final ImagePicker _picker = ImagePicker();
       XFile? image = await _picker.pickImage(source: ImageSource.gallery);
       if (image != null) {
-        var f = await image.readAsBytes();
+        var f= await image.readAsBytes();
         setState(() {
           webImage = f;
           _pickedImage = File('a');
@@ -75,13 +77,12 @@ class MyApp extends State<Widget1> {
   }
 
   Future<void> _sendImageToServer(File imageFile) async {
-    var url = Uri.parse('ссылка на наш сервер'); //вот сюда ссылку на наш сервер
+    var url = Uri.parse('http://localhost:8000/upload'); //вот сюда ссылку на наш сервер
     var request = http.MultipartRequest('POST', url);
-    request.files
-        .add(await http.MultipartFile.fromPath('image', imageFile.path));
+    request.files.add(await http.MultipartFile.fromPath('image', imageFile.path));
 
     var response = await request.send();
-
+    resp = response as String;
     if (response.statusCode == 200) {
       log('Изображение успешно загружено');
     } else {
@@ -89,38 +90,10 @@ class MyApp extends State<Widget1> {
     }
   }
 
-  Future<void> _makeJSON() async {
-    Map<String, dynamic> data = {
-      'image': _pickedImage,
-      // 'text': textFieldValue,
-      // 'category': selectedItem,
-    };
-    if (_pickedImage != null /*&& textFieldValue != ''*/) {
-      String jsonData = json.encode(data);
-      final file = File('data.json');
-      await file.writeAsString(jsonData);
-      _sendJSONToServer(file);
-    }
-  }
-
-  Future<void> _sendJSONToServer(File json) async {
-    var url = Uri.parse('ссылка на наш сервер'); //вот сюда ссылку на наш сервер
-    var request = http.MultipartRequest('POST', url);
-    request.files.add(await http.MultipartFile.fromPath('json', json.path));
-
-    var response = await request.send();
-
-    if (response.statusCode == 200) {
-      log('JSON успешно отправлен');
-    } else {
-      log('Не удалось отправить JSON. Ошибка: ${response.reasonPhrase}');
-    }
-  }
-
   void _processServerResponse(String response) {
     Map<String, dynamic> responseData = json.decode(response);
     String text = responseData['text'];
-    List<String> photos = List<String>.from(responseData['photos']);
+    List<String> photos = List<String>.from(responseData['image_requests']);
     setState(() {
       serverText = text;
       serverPhotos = photos;
@@ -208,7 +181,7 @@ class MyApp extends State<Widget1> {
                         ),
                         ElevatedButton.icon(
                           onPressed: () {
-                            _makeJSON();
+                            _processServerResponse(resp);
                           },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Color.fromRGBO(143, 124, 112, 1),
@@ -235,46 +208,25 @@ class MyApp extends State<Widget1> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          SizedBox(height: 20),
+                          SizedBox(height: 30),
                           Container(
-                            height: 50,
-                            width: 250,
                             decoration: BoxDecoration(
                               color: Color.fromRGBO(143, 124, 112, 1),
-                              border: Border.all(
-                                  color: Color.fromRGBO(0, 0, 0, 0.5)),
-                              borderRadius: BorderRadius.circular(16.0),
+                              borderRadius: BorderRadius.circular(30.0),
                             ),
-                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child:
                           Text(
                             serverText,
                             style: TextStyle(
-                              fontSize: 20.0,
+                              fontSize: 26.0,
                               color: Colors.white,
                               fontFamily: "Montserrat",
+                              ),),
                             ),
                           ),
-
-                          // child: DropdownButton<String>(         //КАТЕГОРИИ
-                          //   style: TextStyle(
-                          //     fontSize: 30.0,
-                          //     color: Color(0xffffffff),
-                          //     fontFamily: "Montserrat",
-                          //     // fontWeight: FontWeight.w600,
-                          //     letterSpacing: 1.0,
-                          //   ),
-                          //   icon: const Icon(
-                          //     Icons.arrow_drop_down,
-                          //     color: Colors.white70,
-                          //   ),
-                          //   dropdownColor: Color.fromRGBO(143, 124, 112, 1),
-                          //   elevation: 2,
-                          //   underline: SizedBox(),
-                          //   items: items.map((item) => DropdownMenuItem<String>(value: item, child: Text(item))).toList(),
-                          //   value: selectedItem,
-                          //   onChanged: (String? newValue) {setState(() {selectedItem = newValue!;}); },
-                          // ),
-
+                          SizedBox(height: 5),
                           SizedBox(
                             width: 130,
                             height: 60,
@@ -286,33 +238,24 @@ class MyApp extends State<Widget1> {
                                       fit: BoxFit.cover)),
                             ),
                           ),
-                          SizedBox(
-                              width: 350,
-                              height: 300,
-                              child: TextField(
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.white,
-                                      fontFamily: "Montserrat"),
-                                  onChanged: (text) {
-                                    setState(() {
-                                      textFieldValue = text;
-                                    });
-                                  },
-                                  decoration: InputDecoration(
-                                      hintText: "Начните вводить описание...",
-                                      hintStyle: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.white70,
-                                        fontFamily: "Montserrat",
-                                      ),
-                                      contentPadding: EdgeInsets.symmetric(
-                                          vertical: 30.0, horizontal: 10.0),
-                                      fillColor: Color.fromRGBO(0, 0, 0, 0.50),
-                                      filled: true,
-                                      border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(30.0))))),
+                          Container(
+                            width: 350,
+                            decoration: BoxDecoration(
+                              color: Color.fromRGBO(0, 0, 0, 0.50),
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child:
+                              Text(
+                                serverDesc,
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Colors.white70,
+                                  fontFamily: "Montserrat",
+                                ),),
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -320,6 +263,7 @@ class MyApp extends State<Widget1> {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  SizedBox(height: 40),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: List.generate(
@@ -333,9 +277,6 @@ class MyApp extends State<Widget1> {
                             width: 160.0,
                             height: 125.0,
                             decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Color.fromRGBO(0, 0, 0, 0.50),
-                              ),
                               color: Color.fromRGBO(0, 0, 0, 0.50),
                               borderRadius: BorderRadius.circular(16.0),
                             ),
@@ -361,9 +302,6 @@ class MyApp extends State<Widget1> {
                             width: 160.0,
                             height: 125.0,
                             decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Color.fromRGBO(0, 0, 0, 0.50),
-                              ),
                               color: Color.fromRGBO(0, 0, 0, 0.50),
                               borderRadius: BorderRadius.circular(16.0),
                             ),
